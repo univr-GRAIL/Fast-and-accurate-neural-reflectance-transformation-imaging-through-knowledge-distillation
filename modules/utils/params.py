7e265493a7721e9054e92680873b51212c212bb3
Tinsae Gebrechristos Dulecha <64842989+tgdulecha@users.noreply.github.com>
2025-11-14 10:14:25 +0100

    Remove dataset, model, relight, and utils modules
    Deleted all files in the dataset, model, relight, and utils modules, as well as all output files for Student and Teacher. This removes all core code and output artifacts, likely as part of a major refactor or project decommission.

--- modules/utils/params.py
+++
@@ -1,6 +0,0 @@
-comp_coeff = 9  # latent features, number of code
-batch_size = 64  # batch size
-num_workers = 8  # number of workers for concurrent data loading
-max_epochs = 100  # number of epochs
-bit_feat = 8  # parameters used for feature encoding.  use default value
-alpha = 0.6  # alpha, weight,  values for distillation loss function.
